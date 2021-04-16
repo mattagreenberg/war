e9448f656e28d055cc7097460b76afd4a19cec43
mattagreenberg <mattagreenberg1@gmail.com>
2021-04-16 12:07:56 -0400

    backend logic

---
+++ server/controllers/authController.js
@@ -0,0 +1,10 @@
+const { uuid } = require('uuidv4');
+const { query } = require('../models/models');
+
+const authController = {};
+
+authController.setAccessToken = (req, res, next) => {
+  
+};
+
+module.exports = authController;
