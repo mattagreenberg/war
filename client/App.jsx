36861a2b759fc5d187333c30757d7ad501b7c703
mattagreenberg <mattagreenberg1@gmail.com>
2021-04-15 21:13:33 -0400

    ci/cd test

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 function App() {
   return (
-    <div style={{ backgroundColor: 'pink' }}>React Test</div>
+    <div style={{ backgroundColor: 'black' }}>React Test</div>
   );
 }
 
