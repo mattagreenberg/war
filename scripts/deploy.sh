9c3a10c43036a7fc524416c376d316d11bafe27c
mattagreenberg <mattagreenberg1@gmail.com>
2021-04-15 21:22:10 -0400

    ci/cd test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,18 +6,18 @@ aws configure set default.region us-east-2
 # Log in to ECR
 eval $(aws ecr get-login --no-include-email --region us-east-2)
 # Build docker image based on our production Dockerfile
-docker build -t backyardcasino/bc .
+docker build -t backyardcasino/bc-prod .
 # tag the image with the Travis-CI SHA
-docker tag bc:latest 727835028611.dkr.ecr.us-east-2.amazonaws.com/bc:$TRAVIS_COMMIT
+docker tag backyardcasino/bc-prod:latest 727835028611.dkr.ecr.us-east-2.amazonaws.com/bc:$TRAVIS_COMMIT
 # Push built image to ECS
 docker push 727835028611.dkr.ecr.us-east-2.amazonaws.com/bc:$TRAVIS_COMMIT
 # Use the linux sed command to replace the text '<VERSION>' in our Dockerrun file with the Travis-CI SHA key
 sed -i='' "s/<VERSION>/$TRAVIS_COMMIT/" Dockerrun.aws.json
 # Zip up our codebase, along with modified Dockerrun and our .ebextensions directory
-zip -r mm-prod-deploy.zip Dockerrun.aws.json .ebextensions
+zip -r bc-prod-deploy.zip Dockerrun.aws.json .ebextensions
 # Upload zip file to s3 bucket
-aws s3 cp mm-prod-deploy.zip s3://$EB_BUCKET/mm-prod-deploy.zip
+aws s3 cp bc-prod-deploy.zip s3://$EB_BUCKET/bc-prod-deploy.zip
 # Create a new application version with new Dockerrun
-aws elasticbeanstalk create-application-version --application-name bc-dev-hot --version-label $TRAVIS_COMMIT --source-bundle S3Bucket=$EB_BUCKET,S3Key=mm-prod-deploy.zip
+aws elasticbeanstalk create-application-version --application-name bc-dev-hot --version-label $TRAVIS_COMMIT --source-bundle S3Bucket=$EB_BUCKET,S3Key=bc-prod-deploy.zip
 # Update environment to use new version number
 aws elasticbeanstalk update-environment --environment-name bc-dev-hot --version-label $TRAVIS_COMMIT
