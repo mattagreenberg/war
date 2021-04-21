c1c9fefbb7462ed4077fda54215a40f6adf0aa61
mattagreenberg <mattagreenberg1@gmail.com>
2021-04-20 20:14:49 -0400

    fixed all errors for real

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 function App() {
   return (
-    <div style={{ backgroundColor: 'purple' }}>React Test</div>
+    <div style={{ backgroundColor: 'orange' }}>React Test</div>
   );
 }
 
